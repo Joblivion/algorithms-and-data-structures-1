150c43d755da870ff10b7fe347af611faf1ea4af
IGI <hokkert24@gmail.com>
2022-04-15 00:39:21 +0200

    Set elkezdve

--- Linked lists/S1L/set.hpp
+++ Linked lists/S1L/set.hpp
@@ -3,13 +3,17 @@
 
 #include "s1l.hpp"
 
-class set: public s1l {
+class set: private s1l {
     // A pushBack() más lesz. Tartozni fog hozzá egy keresés, hogy a set 
     // növekvő maradjon.
     public:
         E1* findInSet(int data);
         void insertIntoSet(int data);
         void deleteFromSet(int data);
+        void reverse();
+
+        using s1l::s1l;
+        using s1l::~s1l;
 };
 
 #endif
